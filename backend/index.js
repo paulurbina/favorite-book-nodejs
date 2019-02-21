ce475846d1065720e744d65971481398b53a64f7
tonypera <paulurbian@hotmail.com>
2019-02-21 12:22:42 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+
+// Initializations
+const app = express();
+require('./database');
+
+// Settings
+app.set('port', process.env.PORT || 7000);
+
+// Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename (req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname))
+    }
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use('/api/books', require('./routes/books'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+})
---
+++ backend/routes/books.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const router = Router();
+
+const Book = require('../models/Book');
+
+router.get('/', async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+});
+
+router.post('/', async (req, res) => {
+    const { title, author, isbn } = req.body;
+    const newBook = new Book({title, author, isbn})
+    await newBook.save();
+    res.send({msg: 'book save'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({msg: 'Book deleting'});
+});
+
+module.exports = router;
